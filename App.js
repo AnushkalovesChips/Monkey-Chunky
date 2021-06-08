b955fb50692c23d8e0e52f048fcbdd397f61f235
AnushkalovesChips <75308848+AnushkalovesChips@users.noreply.github.com>
2021-06-08 16:54:40 +0530

    Update App.js
    changed the background color

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: '#03f0fc',
   },
   inputBox: {
     marginTop: 50,
